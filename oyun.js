39b3faec13b6693a1085d0a3f41610df0e4e050a
bcca4706-hash <bcca4706@gmail.com>
2026-05-26 22:20:51 +0300

    İlk tertemiz başlangıç

---
+++ oyun.js
@@ -0,0 +1,5 @@
+function startGame() {
+    let grid = document.getElementById('grid');
+    grid.innerHTML = '<div class="letter-box"></div><div class="letter-box"></div><div class="letter-box"></div>';
+    alert("Oyun Başladı! 3 Harfli Kelimeyi Bul.");
+}

cb797b467fe70d33074edbf774307c16d774fa3b
bcca4706-hash <bcca4706@gmail.com>
2026-05-26 23:05:32 +0300

    Sayaç ve oyun mantığı eklendi

--- oyun.js
+++ oyun.js
@@ -1,5 +1,29 @@
+const kelimeler = ["ATA", "BAL", "CAM", "DİL", "ELİ", "FİL", "GÜL", "HAL", "IŞIK", "JEST"];
+let currentLevel = 0;
+let skor = 0;
+let sure = 420; // 7 dakika = 420 saniye
+
 function startGame() {
     let grid = document.getElementById('grid');
-    grid.innerHTML = '<div class="letter-box"></div><div class="letter-box"></div><div class="letter-box"></div>';
-    alert("Oyun Başladı! 3 Harfli Kelimeyi Bul.");
+    grid.innerHTML = '';
+    let secilenKelime = kelimeler[currentLevel];
+    
+    // Kutucukları oluştur
+    for(let harf of secilenKelime) {
+        grid.innerHTML += `<div class="letter-box">?</div>`;
+    }
+    
+    // 7 dakikalık sayacı başlat
+    baslatSayaç();
+}
+
+function baslatSayaç() {
+    let timer = setInterval(() => {
+        sure--;
+        console.log("Kalan süre: " + sure);
+        if(sure <= 0) {
+            clearInterval(timer);
+            alert("Süre bitti! Oyun sona erdi.");
+        }
+    }, 1000);
 }
